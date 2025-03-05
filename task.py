f27ca5e9dca0013892f182eb8d28ebe9b95b2255
ButovStanislav <mkro9989@gmail.com>
2025-03-05 14:00:14 +0300

    qwerty123

--- task.py
+++ task.py
@@ -1,1 +1,13 @@
+def merge_lists_to_dict(list1, list2):
+    combined_data = zip(list1, list2)
+    result_dict = dict(combined_data)
+    return result_dict
 
+products = ["eggs", "apple", "Coke", "Milk"]
+prices = [150, 30, 80, 20]
+
+product_dictionary_kword = merge_lists_to_dict(list1=products, list2=prices)
+print(f"Dictionary using keyword arguments: {product_dictionary_kword}")
+
+product_dictionary_full = merge_lists_to_dict(products, list2=prices)
+print(f"Dictionary using mixed arguments: {product_dictionary_full}")
